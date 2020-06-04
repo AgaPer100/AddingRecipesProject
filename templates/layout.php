f783f5dad7c4af657e1a9224e3ce271a1f5f1d56
Agnieszka Perkowska <agaper100@gmail.com>
2020-06-05 00:32:26 +0200

    added css

--- templates/layout.php
+++ templates/layout.php
@@ -1,31 +1,35 @@
-<html>
-    <head>
-    </head>
-    <body>
-        <div class="header">
-            <h1>Moje przepisy</h11>
-        </div>
-        
-        <div class="container">
-            <div class="menu">
-                <ul>
-                    <li>
-                        <a href="/">Lista przepisów</a>
-                    </li>
-                    <li>
-                        <a href="/?action=create">Nowy przepis</a>
-                    </li>
-                </ul>
-            </div>
-            <div>
-            <?php
-                require_once("templates/pages/$page.php");
-            ?>
-            </div>
-        </div>
+<html lang="pl">
 
-        <div class="footer">
-            STOPKA
+    <head>
+        <title>Moje przepisy</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css">
+        <link rel="stylesheet" href="/public/style.css">
+    </head>
+
+    <body>
+        <div class="wrapper">
+            <div class="header">
+                <h1><i class="far fa-clipboard"></i>Moje przepisy</h11>
+            </div>
+        
+            <div class="container">
+                <div class="menu">
+                    <ul>
+                        <li><a href="/">Strona główna</a></li>
+                        <li><a href="/?action=create">Nowy przepis</a></li>
+                    </ul>
+                </div>
+
+                <div class="page">
+                    <?php require_once("templates/pages/$page.php"); ?>
+                </div>
+            </div>
+
+            <div class="footer">
+                <p>Moja książka z przepisami</p>
+            </div>
         </div>
     </body>
+
 </html>
