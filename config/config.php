f73deb74822152a5b5c953729c8559895355c3e1
Agnieszka Perkowska <agaper100@gmail.com>
2020-06-07 18:06:09 +0200

    added db configuration

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'db' => [
+        'host' => 'localhost',
+        'database' => 'recipes',
+        'user' => 'user_recipes',
+        'password' => 'test1234'
+    ]
+];
--- index.php
+++ index.php
@@ -7,11 +7,14 @@ namespace App;
 require_once("src/Utils/debug.php");
 require_once("src/Controller.php");
 
+$configuration = require_once("config/config.php");
+
 $request = [
     'get' => $_GET,
     'post' => $_POST
 ];
 
+Controller::initConfiguration($configuration);
 (new Controller($request))->run();
 
 
